84e11f9ab9aeaff2a57791001fa6fd891f0556ce
Ruhshan <ruhshan.ahmed@gmail.com>
2020-11-17 19:31:39 +0600

    Create skeleton

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <p>This is home</p>
+  </div>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Home extends Vue {}
+</script>
