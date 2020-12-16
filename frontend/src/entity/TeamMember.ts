0793b2590127030c83ac17339d7a0312df1c220a
Ruhshan <ruhshan.ahmed@gmail.com>
2020-12-17 00:46:46 +0600

    Add team member's info

---
+++ frontend/src/entity/TeamMember.ts
@@ -0,0 +1,14 @@
+class TeamMember {
+    constructor(
+        public id: number,
+        public name: string,
+        public role: string,
+        public affiliation: string,
+        public email: string,
+        public picture: string
+
+    ) {}
+
+}
+
+export default TeamMember;
--- frontend/src/views/Team.vue
+++ frontend/src/views/Team.vue
@@ -14,6 +14,40 @@
 
 		<section class="inner-page">
 
+            <section id="team" class="team section-bg">
+      <div class="container">
+
+<!--        <div class="section-title">-->
+<!--          <h2>Team</h2>-->
+<!--          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>-->
+<!--        </div>-->
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch" v-for="member in teamMembers" v-bind:key="member.id">
+            <div class="member">
+              <img src=@/assets/img/team/team-1.jpg alt="">
+              <h4>{{ member.name }}</h4>
+              <span>{{ member.role }}</span>
+              <p>
+                {{ member.affiliation }}
+              </p>
+              <div class="social">
+                <a :href="member.email"><i class="icofont-mail"></i></a>
+                <a href=""><i class="icofont-facebook"></i></a>
+                <a href=""><i class="icofont-instagram"></i></a>
+                <a href=""><i class="icofont-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+
+
+
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
+
 		</section>
 	</div>
     
@@ -21,9 +55,19 @@
 
 <script lang="ts">
 import { Vue } from "vue-property-decorator";
+import TeamMember from '@/entity/TeamMember';
 
 export default class Team extends Vue{
-
+    teamMembers: Array<TeamMember> =[
+        new TeamMember(1,"M. Arif Ashraf", "Project coordinator", "Postdoctoral Research Associate, Biology department, University of Massachusetts\n" +
+            "Amherst, USA", "mailto:arif.ashraf.opu@gmail.com","/img/team/team-1.jpg"),
+        new TeamMember(2,"Md. Kamrul Hasan", "Project coordinator", "Lecturer, Department of Biochemistry and Molecular Biology\n" +
+            "Tejgaon College, National University of Bangladesh, Gazipur- 1704, Bangladesh", "mailto:kamrulhasanhridoy205@gmail.com","@/assets/img/team/team-1.jpg"),
+        new TeamMember(3,"Nazmul Hussain", "Reseacher", "Tejgaon College, National University of Bangladesh, Gazipur- 1704, Bangladesh", "mailto:nazmulrobs@gmail.com","@/assets/img/team/team-1.jpg"),
+        new TeamMember(4,"Rony Chanda", "Researcher", "Tejgaon College, National University of Bangladesh, Gazipur- 1704, Bangladesh", "mailto:ronychando98@gmail.com","@/assets/img/team/team-1.jpg"),
+        new TeamMember(5,"Ruhshan Ahmed Abir", "Frontend developer", "Bio-Bio-1 Bioinformatics Research Foundation", "mailto:ruhshan.ahmed@@gmail.com","@/assets/img/team/team-1.jpg"),
+        new TeamMember(6,"Mohsina Akter Mou", "Backend developer", "Shahjalal University of Science and Technology, Sylhet", "mailto:ruhshan.ahmed@@gmail.com","@/assets/img/team/team-1.jpg"),
+    ];
 }
 </script>
 
